cc100d9100827741d3c1fce824b0e5f88c2f33b0
ma7ammad <34472849+ma7ammad@users.noreply.github.com>
2020-11-21 17:33:56 +0000

    add logging to a file functionality (#4)
    * added loggin to app
    
    * MA-4 added functionality to log into a file using NLoger

--- Mosaic.API/Program.cs
+++ Mosaic.API/Program.cs
@@ -1,5 +1,9 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using NLog.Web;
+using System;
+
 
 namespace Mosaic.API
 {
@@ -7,7 +11,27 @@ namespace Mosaic.API
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            //To log errors on bootstrapping 
+            // logger can not be injected as as DI container has not been set up yet
+            //therefore use built in statement GetCurrentClassLogger() that provides an
+            // NLog-logger for the program.cs class
+            var logger = NLog.Web.NLogBuilder.ConfigureNLog("nlog.config").GetCurrentClassLogger();
+            try
+            {
+                logger.Info("**************************** Initialising Mosaic.API ****************************");
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception exception)
+            {
+                //NLog: catch setup errors
+                logger.Error(exception, "Stopped program because of exception");
+                throw;
+            }
+            finally
+            {
+                // Ensure to flush and stop internal timers/threads before application-exit (Avoid segmentation fault on Linux)
+                NLog.LogManager.Shutdown();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
@@ -15,6 +39,12 @@ namespace Mosaic.API
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
-                });
+                })
+                .ConfigureLogging(logging =>
+                {
+                    logging.ClearProviders();
+                    logging.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Trace);
+                })
+                .UseNLog();  // NLog: Setup NLog for Dependency injection
     }
 }
