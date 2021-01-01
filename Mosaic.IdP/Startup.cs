d0c4c3c19fb505c399649e76c38bcd658ffbe861
ma7ammad <kakadumdum@yandex.com>
2021-01-01 22:29:24 +0000

    Saving IdP config to (docker) mssql server localdb (container)

--- Mosaic.IdP/Startup.cs
+++ Mosaic.IdP/Startup.cs
@@ -2,12 +2,17 @@
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
 
+using IdentityServer4.EntityFramework.DbContexts;
+using IdentityServer4.EntityFramework.Mappers;
 using IdentityServerHost.Quickstart.UI;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
+using System.Linq;
+using System.Reflection;
 using System.Security.Cryptography.X509Certificates;
 
 namespace Mosaic.IdP
@@ -23,6 +28,8 @@ namespace Mosaic.IdP
 
         public void ConfigureServices(IServiceCollection services)
         {
+            // hardcoding connection string code-first 
+            var mosaicidpdbconnectionstring = "Server=tcp:localhost;Database=MosaicIdPDB;User Id=SA;Password=Pass_word;Integrated Security=False;MultipleActiveResultSets=true;";
             // uncomment, if you want to add an MVC-based UI
             services.AddControllersWithViews();
 
@@ -31,15 +38,34 @@ namespace Mosaic.IdP
                 // see https://identityserver4.readthedocs.io/en/latest/topics/resources.html
                 options.EmitStaticAudienceClaim = true;
             })
-                .AddInMemoryIdentityResources(Config.IdentityResources)
-                .AddInMemoryApiScopes(Config.ApiScopes)
-                .AddInMemoryApiResources(Config.ApiResources)
-                .AddInMemoryClients(Config.Clients)
+                //.AddInMemoryIdentityResources(Config.IdentityResources)
+                //.AddInMemoryApiScopes(Config.ApiScopes)
+                //.AddInMemoryApiResources(Config.ApiResources)
+                //.AddInMemoryClients(Config.Clients)
                 .AddTestUsers(TestUsers.Users);
 
             // not recommended for production - you need to store your key material somewhere secure
             builder.AddDeveloperSigningCredential();
             //builder.AddSigningCredential(LoadCertificateFromStore());
+
+            var migrationsAssembly = typeof(Startup).GetTypeInfo().Assembly.GetName().Name;
+
+            builder.AddConfigurationStore(options =>
+            {
+                options.ConfigureDbContext = builder =>
+                    builder.UseSqlServer(mosaicidpdbconnectionstring, options => 
+                    {
+                        //tell EntityCore/sql that migration assebly are different and located in MosaicIdP assembly
+                        options.MigrationsAssembly(migrationsAssembly);
+                    });
+            });
+
+            builder.AddOperationalStore(options =>
+            {
+                options.ConfigureDbContext = builder =>
+                    builder.UseSqlServer(mosaicidpdbconnectionstring,
+                    options => options.MigrationsAssembly(migrationsAssembly));
+            });
         }
 
         public void Configure(IApplicationBuilder app)
@@ -48,6 +74,9 @@ namespace Mosaic.IdP
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            // Load DB
+            InitializeDatabase(app);
 
             // uncomment if you want to add MVC
             app.UseStaticFiles();
@@ -78,6 +107,55 @@ namespace Mosaic.IdP
                 }
 
                 return certCollection[0];
+            }
+        }
+
+        private void InitializeDatabase(IApplicationBuilder app)
+        {
+            using (var serviceScope = app.ApplicationServices
+                .GetService<IServiceScopeFactory>().CreateScope()) // to run in current scope
+            {
+                serviceScope.ServiceProvider
+                    .GetRequiredService<PersistedGrantDbContext>().Database.Migrate();
+
+                var context = serviceScope.ServiceProvider
+                    .GetRequiredService<ConfigurationDbContext>();
+                context.Database.Migrate();
+                if (!context.Clients.Any())
+                {
+                    foreach (var client in Config.Clients)
+                    {
+                        context.Clients.Add(client.ToEntity());
+                    }
+                    context.SaveChanges();
+                }
+
+                if (!context.IdentityResources.Any())
+                {
+                    foreach (var resource in Config.IdentityResources)
+                    {
+                        context.IdentityResources.Add(resource.ToEntity());
+                    }
+                    context.SaveChanges();
+                }
+
+                if (!context.ApiScopes.Any())
+                {
+                    foreach (var resource in Config.ApiScopes)
+                    {
+                        context.ApiScopes.Add(resource.ToEntity());
+                    }
+                    context.SaveChanges();
+                }
+
+                if (!context.ApiResources.Any())
+                {
+                    foreach (var resource in Config.ApiResources)
+                    {
+                        context.ApiResources.Add(resource.ToEntity());
+                    }
+                    context.SaveChanges();
+                }
             }
         }
     }
