b10d76a7f7356730e8545427d156ebb8e28888de
ma7ammad <kakadumdum@yandex.com>
2021-01-16 14:47:18 +0000

    Add users db in IdP project

--- Mosaic.IdP/Startup.cs
+++ Mosaic.IdP/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Mosaic.IdP.DbContexts;
 using System;
 using System.Linq;
 using System.Reflection;
@@ -28,44 +29,49 @@ namespace Mosaic.IdP
 
         public void ConfigureServices(IServiceCollection services)
         {
-            // hardcoding connection string code-first 
-            var mosaicidpdbconnectionstring = "Server=tcp:localhost;Database=MosaicIdPDB;User Id=SA;Password=Pass_word;Integrated Security=False;MultipleActiveResultSets=true;";
             // uncomment, if you want to add an MVC-based UI
             services.AddControllersWithViews();
 
+            // hardcoding connection string code-first 
+            var mosaicidpdbconnectionstring = "Server=tcp:localhost;Database=MosaicIdPDB;User Id=SA;Password=Pass_word;Integrated Security=False;MultipleActiveResultSets=true;";
+
+            services.AddDbContext<IdentityDbContext>(options =>
+            {
+                options.UseSqlServer(mosaicidpdbconnectionstring);
+            });
             var builder = services.AddIdentityServer(options =>
             {
                 // see https://identityserver4.readthedocs.io/en/latest/topics/resources.html
                 options.EmitStaticAudienceClaim = true;
             })
-                //.AddInMemoryIdentityResources(Config.IdentityResources)
-                //.AddInMemoryApiScopes(Config.ApiScopes)
-                //.AddInMemoryApiResources(Config.ApiResources)
-                //.AddInMemoryClients(Config.Clients)
-                .AddTestUsers(TestUsers.Users);
+                .AddInMemoryIdentityResources(Config.IdentityResources)
+                .AddInMemoryApiScopes(Config.ApiScopes)
+                .AddInMemoryApiResources(Config.ApiResources)
+                .AddInMemoryClients(Config.Clients);
+                //.AddTestUsers(TestUsers.Users);
 
             // not recommended for production - you need to store your key material somewhere secure
             builder.AddDeveloperSigningCredential();
             //builder.AddSigningCredential(LoadCertificateFromStore());
 
-            var migrationsAssembly = typeof(Startup).GetTypeInfo().Assembly.GetName().Name;
+            //var migrationsAssembly = typeof(Startup).GetTypeInfo().Assembly.GetName().Name;
 
-            builder.AddConfigurationStore(options =>
-            {
-                options.ConfigureDbContext = builder =>
-                    builder.UseSqlServer(mosaicidpdbconnectionstring, options => 
-                    {
-                        //tell EntityCore/sql that migration assebly are different and located in MosaicIdP assembly
-                        options.MigrationsAssembly(migrationsAssembly);
-                    });
-            });
+            //builder.AddConfigurationStore(options =>
+            //{
+            //    options.ConfigureDbContext = builder =>
+            //        builder.UseSqlServer(mosaicidpdbconnectionstring, options =>
+            //        {
+            //            //tell EntityCore/sql that migration assebly are different and located in MosaicIdP assembly
+            //            options.MigrationsAssembly(migrationsAssembly);
+            //        });
+            //});
 
-            builder.AddOperationalStore(options =>
-            {
-                options.ConfigureDbContext = builder =>
-                    builder.UseSqlServer(mosaicidpdbconnectionstring,
-                    options => options.MigrationsAssembly(migrationsAssembly));
-            });
+            //builder.AddOperationalStore(options =>
+            //{
+            //    options.ConfigureDbContext = builder =>
+            //        builder.UseSqlServer(mosaicidpdbconnectionstring,
+            //        options => options.MigrationsAssembly(migrationsAssembly));
+            //});
         }
 
         public void Configure(IApplicationBuilder app)
@@ -76,7 +82,7 @@ namespace Mosaic.IdP
             }
 
             // Load DB
-            InitializeDatabase(app);
+            //InitializeDatabase(app);
 
             // uncomment if you want to add MVC
             app.UseStaticFiles();
