2a0a089d152e3ac931c2e055470bb9be382f3099
ma7ammad <kakadumdum@yandex.com>
2021-01-16 15:44:39 +0000

    switched to using the users db instead of hardcoded testusers

--- Mosaic.IdP/Quickstart/TestUsers.cs
+++ Mosaic.IdP/Quickstart/TestUsers.cs
@@ -10,36 +10,36 @@ namespace IdentityServerHost.Quickstart.UI
 {
     public class TestUsers
     {
-        public static List<TestUser> Users = new List<TestUser>
-        {
-            new TestUser
-            {
-                SubjectId = "d860efca-22d9-47fd-8249-791ba61b07c7",
-                Username = "Frank",
-                Password = "password",
+        //public static List<TestUser> Users = new List<TestUser>
+        //{
+            //new TestUser
+            //{
+            //    SubjectId = "d860efca-22d9-47fd-8249-791ba61b07c7",
+            //    Username = "Frank",
+            //    Password = "password",
 
-                Claims = new List<Claim>
-                {
-                    new Claim("given_name", "Frank"),
-                    new Claim("family_name", "Underwood"),
-                    new Claim("email", "Hogwarts@email.com"),
-                    new Claim("role", "Admin"),
-                }
-            },
-            new TestUser
-            {
-                SubjectId = "b7539694-97e7-4dfe-84da-b4256e1ff5c7",
-                Username = "Claire",
-                Password = "password",
+            //    Claims = new List<Claim>
+            //    {
+            //        new Claim("given_name", "Frank"),
+            //        new Claim("family_name", "Underwood"),
+            //        new Claim("email", "Hogwarts@email.com"),
+            //        new Claim("role", "Admin"),
+            //    }
+            //},
+            //new TestUser
+            //{
+            //    SubjectId = "b7539694-97e7-4dfe-84da-b4256e1ff5c7",
+            //    Username = "Claire",
+            //    Password = "password",
 
-                Claims = new List<Claim>
-                {
-                    new Claim("given_name", "Claire"),
-                    new Claim("family_name", "Underwood"),
-                    new Claim("email", "Dumbledore@email.com"),
-                    new Claim("role", "Customer"),
-                }
-            }
+            //    Claims = new List<Claim>
+            //    {
+            //        new Claim("given_name", "Claire"),
+            //        new Claim("family_name", "Underwood"),
+            //        new Claim("email", "Dumbledore@email.com"),
+            //        new Claim("role", "Customer"),
+            //    }
+            //}
             //get
             //{
             //    var address = new
@@ -86,6 +86,6 @@ namespace IdentityServerHost.Quickstart.UI
             //        }
             //    };
             //}
-        };
+        //};
     }
 }
