c77e3115d3ef8c69788223d6c8d1768da7ffd03f
ma7ammad <kakadumdum@yandex.com>
2021-01-16 15:47:31 +0000

    Cleanup

--- Mosaic.IdP/Quickstart/TestUsers.cs
+++ Mosaic.IdP/Quickstart/TestUsers.cs
@@ -2,9 +2,9 @@
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
 
-using IdentityServer4.Test;
-using System.Collections.Generic;
-using System.Security.Claims;
+//using IdentityServer4.Test;
+//using System.Collections.Generic;
+//using System.Security.Claims;
 
 namespace IdentityServerHost.Quickstart.UI
 {
